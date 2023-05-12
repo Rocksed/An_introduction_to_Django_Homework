dfa0b0ec4fa020387b2183d1c2f3d2a5870c0f9a
ilyag <ilyagotin@mail.ru>
2023-05-12 17:28:01 +0300

    Template. Homework. update

--- catalog/views.py
+++ catalog/views.py
@@ -10,9 +10,11 @@ def index(request):
     }
     return render(request, 'catalog/index.html', context, )
 
-# if request.method == "POST":
-# name = request.POST.get('name')
-# email = request.POST.get('email')
-# massage = request.POST.get('massage')
-# print(f'User_name: {name}, User_email: {email}, User_massage:{massage}')
-# return render(request, 'catalog/index.html')
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        massage = request.POST.get('massage')
+        print(f'User_name: {name}, User_email: {email}, User_massage:{massage}')
+    return render(request, 'catalog/cast.html',)
